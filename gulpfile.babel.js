057793272afb4614df5d21a6cd30a7ebb4619725
Casey White <casey.r.white@gmail.com>
2015-07-30 00:39:38 -0500

    add tests

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,7 @@
-import gulp   from 'gulp';
-import babel  from 'babel/register';
+import gulp from 'gulp';
+import babel from 'babel/register';
 import eslint from 'gulp-eslint';
-import mocha  from 'gulp-mocha';
+import mocha from 'gulp-mocha';
 
 gulp.task('lint', () => {
   return gulp.src(['./jsxml.js', './test/**/*.js'])
---
+++ test/jsxml.js
@@ -0,0 +1,25 @@
+import jsdom from 'mocha-jsdom';
+import chai, { assert, expect } from 'chai';
+import jsxml from '../jsxml';
+
+/** @jsx jsxml */
+
+describe('jsxml', () => {
+  // Load browser API
+  jsdom();
+
+  it('should return an HTMLElement', () => {
+    const ComponentP = (
+      <p className='test-p'>This is a test</p>
+    );
+
+    const ComponentDiv = (
+      <div className='test-div'><ComponentP /></div>
+    );
+
+    expect(ComponentP).to.be.an.instanceof(HTMLElement);
+    expect(ComponentDiv).to.be.an.instanceof(HTMLElement);
+
+    assert.equal(ComponentDiv.querySelector('p').className, 'test-p');
+  });
+});
