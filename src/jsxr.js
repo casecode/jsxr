12433d14a9146b67e89fe9786c724cdc5947023d
Casey White <casey.r.white@gmail.com>
2015-08-17 21:44:32 -0500

    clone components

--- src/jsxr.js
+++ src/jsxr.js
@@ -15,7 +15,7 @@ function jsxr(tag, attributes, ...children) {
   // ensure children is a flattened array
   const childElements = [].concat(...children);
 
-  const element = tag instanceof HTMLElement ? tag : document.createElement(tag);
+  const element = tag instanceof HTMLElement ? tag.cloneNode(true) : document.createElement(tag);
 
   const keys = Object.keys(attrs);
   for (let i = 0; i < keys.length; i++) {
@@ -38,7 +38,7 @@ function jsxr(tag, attributes, ...children) {
 
   for (let i = 0; i < childElements.length; i++) {
     const child = childElements[i];
-    
+
     element.appendChild(
       child instanceof HTMLElement ?
         child :
